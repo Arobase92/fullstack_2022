8b382d54d22eceb5dfe3531ff1530a6dbfd798b8
arobase <sergedelonboka92@gmail.com>
2022-09-30 01:26:33 +0000

    courseinfo3 modification des variables du composant App et refactorisation

--- part1/courseinfo/src/Total.js
+++ part1/courseinfo/src/Total.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const Total = ({total}) => {
+const Total = ({part1, part2, part3}) => {
+	const total = part1.exercises + part2.exercises + part3.exercises
 	return (
 		<div>
 			<p> Number of exercises {total} </p>
